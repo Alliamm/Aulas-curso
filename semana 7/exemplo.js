daed7584d9f4dd03f84341deff272e43bb6c845f
Alliamm <pfingstagallan@gmail.com>
2026-08-19 10:23:33 -0300

    jS cansa bicho

---
+++ semana 7/exemplo.js
@@ -0,0 +1,9 @@
+for(var i = 0;i < 16; i++){
+    var idade = Number(prompt("Digite sua idade"));
+    if(idade >= 18){
+        write("Você é maior de idade");
+    }
+    else {
+        write("Você é maior de idade")
+    }
+}
